e217faa44b8e46bc887a632a06eebbac1d547903
Azizbek <khasanjonovich@gmail.com>
2023-05-16 23:15:33 +0900

    assignment 2

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ java --version
 mvn --version
 git clone https://github.com/typosbro/cse364.git
 cd cse364/
+git checkout milestone2
+mvn jacoco:report
 mvn package
 java -jar target/cse364-project-1.0-SNAPSHOT.jar
